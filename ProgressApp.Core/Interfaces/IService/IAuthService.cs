9356bc56bcee9121f9a9f26bbfb154173aee6d1a
Yurii <yurachellenuk@gmail.com>
2026-04-07 22:51:37 +0300

    feat: initial services for v1.3.0

---
+++ ProgressApp.Core/Interfaces/IService/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ProgressApp.Core.Interfaces.IService
+{
+    public interface IAuthService
+    {
+        bool IsDatabaseCreated();
+        Task<bool> LoginAsync(string password);
+        Task<bool> RegisterAsync(string password);
+        Task ChangePasswordAsync(string newPassword);
+        
+        Task RemovePasswordAsync();
+    }
+}
