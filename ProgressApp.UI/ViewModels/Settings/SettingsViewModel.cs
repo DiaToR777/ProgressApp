63fc2f61526828c870ee42792509c6c093f713ca
Yurii <yurachellenuk@gmail.com>
2026-05-05 11:05:55 +0300

    ref: migrate SettingsViewModel to CommunityToolkit.Mvvm

--- ProgressApp.UI/ViewModels/Settings/SettingsViewModel.cs
+++ ProgressApp.UI/ViewModels/Settings/SettingsViewModel.cs
@@ -1,16 +1,15 @@
-﻿using ProgressApp.Core.Exceptions;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ProgressApp.Core.Exceptions;
 using ProgressApp.Core.Interfaces.IService;
 using ProgressApp.Core.Models.Enums;
 using ProgressApp.Core.Models.Localization;
 using Serilog;
-using System.Windows.Input;
 
 namespace ProgressApp.WpfUI.ViewModels.Settings
 {
-    public class SettingsViewModel : ViewModelBase
+    public partial class SettingsViewModel : ObservableObject
     {
-        public List<LanguageModel> AvailableLanguages => LanguageConfig.AvailableLanguages;
-
         private readonly ISettingsService _settingsService;
         private readonly IMessageService _messageService;
         private readonly IAppConfigService _appConfigService;
@@ -19,87 +18,49 @@ namespace ProgressApp.WpfUI.ViewModels.Settings
         private readonly IAppThemeService _themeService;
         private readonly IAuthService _authService;
 
-        private string _username = string.Empty;
-        private string _goal = string.Empty;
-        private AppTheme _selectedTheme;
-        private LanguageModel _selectedLanguage;
+        public List<LanguageModel> AvailableLanguages => LanguageConfig.AvailableLanguages;
+        public Array AllThemes => Enum.GetValues(typeof(AppTheme));
 
-        private string _newDbPassword = string.Empty;
-        private string _confirmDbPassword = string.Empty;
-        private bool _isChangingPassword;
+        [ObservableProperty]
         private bool _isDbEncrypted;
-        public bool IsDbEncrypted
-        {
-            get => _isDbEncrypted;
-            set => SetProperty(ref _isDbEncrypted, value);
-        }
 
-        public bool IsChangingPassword
-        {
-            get => _isChangingPassword;
-            set
-            {
-                if (SetProperty(ref _isChangingPassword, value))
-                    OnPropertyChanged(nameof(IsNotChangingPassword));
-            }
-        }
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotChangingPassword))]
+        private bool _isChangingPassword;
 
         public bool IsNotChangingPassword => !IsChangingPassword;
 
-        public string NewDbPassword
-        {
-            get => _newDbPassword;
-            set => SetProperty(ref _newDbPassword, value);
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(ApplyNewPasswordCommand))]
+        private string _newDbPassword = string.Empty;
 
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(ApplyNewPasswordCommand))]
+        private string _confirmDbPassword = string.Empty;
+
+        [ObservableProperty]
+        private LanguageModel _selectedLanguage;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SaveSettingsCommand))]
+        private string _username = string.Empty;
+
+        partial void OnUsernameChanged(string value)
+        {
+            if (value?.Length > 50) Username = value.Substring(0, 50);
         }
 
-        public string ConfirmDbPassword
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SaveSettingsCommand))]
+        private string _goal = string.Empty;
+
+        partial void OnGoalChanged(string value)
         {
-            get => _confirmDbPassword;
-            set => SetProperty(ref _confirmDbPassword, value);
+            if (value?.Length > 150) Goal = value.Substring(0, 150);
         }
 
-
-        public LanguageModel SelectedLanguage
-        {
-            get => _selectedLanguage;
-            set => SetProperty(ref _selectedLanguage, value);
-        }
-
-        public string Username
-        {
-            get => _username;
-            set
-            {
-                var _sanitizedValue = value?.Length > 50 ? value.Substring(0, 50) : value;
-                SetProperty(ref _username, _sanitizedValue);
-            }
-        }
-
-        public string Goal
-        {
-            get => _goal;
-            set
-            {
-                var _sanitizedValue = value?.Length > 150 ? value.Substring(0, 150) : value;
-                SetProperty(ref _goal, _sanitizedValue);
-            }
-        }
-
-        public AppTheme SelectedTheme
-        {
-            get => _selectedTheme;
-            set => SetProperty(ref _selectedTheme, value);
-        }
-
-        public Array AllThemes => Enum.GetValues(typeof(AppTheme));
-
-        public ICommand SaveSettingsCommand { get; }
-        public ICommand ExportEntriesCommand { get; }
-        public ICommand ImportEntriesCommand { get; }
-        public ICommand TogglePasswordFieldsCommand { get; }
-        public ICommand ApplyNewPasswordCommand { get; }
-        public ICommand RemovePasswordCommand { get; }
+        [ObservableProperty]
+        private AppTheme _selectedTheme;
 
         public SettingsViewModel(
             ISettingsService settingsService,
@@ -118,164 +79,10 @@ namespace ProgressApp.WpfUI.ViewModels.Settings
             _localizationService = localizationService;
             _themeService = themeService;
 
-            Initialize();
-
-            SaveSettingsCommand = new RelayCommand(
-                executeAsync: async _ => { await SaveSettings(); },
-                canExecute: _ => !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Goal));
-
-            ExportEntriesCommand = new RelayCommand(async _ => await ExportEntriesAsync());
-
-            ImportEntriesCommand = new RelayCommand(executeAsync: async _ => await ImportEntriesAsync());
-
-            ApplyNewPasswordCommand = new RelayCommand(
-                executeAsync: async _ => await ApplyNewPassword(),
-                canExecute: _ => !string.IsNullOrWhiteSpace(NewDbPassword) && NewDbPassword == ConfirmDbPassword
-            );
-
-            TogglePasswordFieldsCommand = new RelayCommand(_ => IsChangingPassword = !IsChangingPassword);
-
-            RemovePasswordCommand = new RelayCommand(
-                executeAsync: async _ => await RemovePasswordAsync(),
-                canExecute: _ => IsDbEncrypted);
+            _ = InitializeAsync();
         }
 
-        private async Task RemovePasswordAsync()
-        {
-            var confirmed = await _messageService.ShowConfirmationAsync("Msg_RemovePasswordConfirmation");
-            if (!confirmed) return;
-            try
-            {
-                await _authService.RemovePasswordAsync();
-                IsDbEncrypted = false;
-                Log.Debug("SettingsVM: IsDbEncrypted = {Value}", IsDbEncrypted);
-                await _messageService.ShowInfoAsync("Msg_PasswordRemovedSuccess");
-            }
-            catch (AppException ex)
-            {
-                Log.Error(ex, "SettingsVM: Error removing password");
-                await _messageService.ShowErrorAsync(ex);
-            }
-        }
-        private async Task ApplyNewPassword()
-        {
-            try
-            {
-                if (IsDbEncrypted)
-                {
-                    Log.Information("SettingsVM: Changing DB password to a new one.");
-
-                    await _authService.ChangePasswordAsync(NewDbPassword);
-
-                    await _messageService.ShowInfoAsync("Msg_PasswordChangedSuccess");
-
-                    NewDbPassword = string.Empty;
-                    ConfirmDbPassword = string.Empty;
-                    IsChangingPassword = false;
-                }
-                else
-                {
-                    Log.Information("SettingsVM: Setting new DB password for unencrypted database.");
-                    await _authService.SetPasswordAsync(NewDbPassword);
-                    IsDbEncrypted = true;
-
-                    Log.Debug("SettingsVM: IsDbEncrypted = {Value}", IsDbEncrypted);
-                    await _messageService.ShowInfoAsync("Msg_PasswordSetSuccess");
-
-                    NewDbPassword = string.Empty;
-                    ConfirmDbPassword = string.Empty;
-                    IsChangingPassword = false;
-                }
-            }
-            catch (AppException ex)
-            {
-                Log.Error(ex, "SettingsVM: Password change failed");
-                await _messageService.ShowErrorAsync(ex);
-            }
-        }
-
-        private async Task ImportEntriesAsync()
-        {
-            Log.Information("SettingsVM: Starting import process... Getting file path from user.");
-
-            var path = _messageService.OpenFileDialog("CSV files (*.csv)|*.csv");
-
-            if (string.IsNullOrEmpty(path))
-            {
-                Log.Information("SettingsVM: Import cancelled by user.");
-                return;
-            }
-
-            var confirmed = await _messageService.ShowConfirmationAsync("Msg_ImportConfirmation");
-
-            if (!confirmed) return;
-
-            try
-            {
-                var importedCount = await _dataExchangeService.ImportFromCsvAsync(path);
-                await _messageService.ShowInfoAsync("Msg_SuccessImportInfo", importedCount);
-            }
-            catch (AppException ex)
-            {
-                Log.Error(ex, "SettingsVM: Import failed");
-                await _messageService.ShowErrorAsync(ex);
-            }
-        }
-        private async Task ExportEntriesAsync()
-        {
-            try
-            {
-                Log.Information("SettingsVM: Starting export process... Getting file path from user.");
-                var path = _messageService.SaveFileDialog(
-                    $"Progress_Backup_{DateTime.Now:yyyyMMdd}",
-                    "CSV files (*.csv)|*.csv"
-                );
-
-                if (string.IsNullOrEmpty(path))
-                {
-                    Log.Information("SettingsVM: Export cancelled by user.");
-                    return;
-                }
-
-                Log.Information("SettingsVM: Exporting data to {FilePath}", path);
-                await _dataExchangeService.ExportToCsvAsync(path);
-
-                await _messageService.ShowInfoAsync("Msg_SuccessExportInfo");
-            }
-            catch (AppException ex)
-            {
-                Log.Error(ex, "SettingsVM: Error exporting entries");
-                await _messageService.ShowErrorAsync(ex);
-            }
-        }
-        private async Task SaveSettings()
-        {
-            try
-            {
-                Log.Information("SettingsVM: Saving settings. New Culture: {Culture}, Theme: {Theme}",
-                    SelectedLanguage.CultureCode, SelectedTheme);
-
-                var config = _appConfigService.Load();
-
-                config.Theme = SelectedTheme.ToString();
-                config.Language = SelectedLanguage.CultureCode;
-                config.Username = Username;
-                _appConfigService.Save(config);
-
-                await _settingsService.SaveGoalAsync(Goal);
-                _localizationService.ChangeLanguage(SelectedLanguage.CultureCode);
-                _themeService.SetTheme(SelectedTheme);
-
-                await _messageService.ShowInfoAsync("Msg_SettingsSaved");
-                Log.Information("SettingsVM: Settings updated successfully.");
-            }
-            catch (AppException ex)
-            {
-                Log.Error(ex, "SettingsVM: Error saving settings");
-                await _messageService.ShowErrorAsync(ex);
-            }
-        }
-        private async void Initialize()
+        private async Task InitializeAsync()
         {
             try
             {
@@ -289,15 +96,126 @@ namespace ProgressApp.WpfUI.ViewModels.Settings
                 SelectedTheme = Enum.Parse<AppTheme>(config.Theme);
                 SelectedLanguage = LanguageConfig.GetByCode(config.Language);
 
-                Log.Debug("SettingsVM: All settings loaded in parallel");
+                Log.Debug("SettingsVM: All settings loaded");
             }
             catch (AppException ex)
             {
-                Log.Error(ex, "SettingsVM: Error while getting settings");
+                Log.Error(ex, "SettingsVM: Error loading settings");
                 await _messageService.ShowErrorAsync(ex);
             }
         }
 
+        [RelayCommand(CanExecute = nameof(CanSaveSettings))]
+        private async Task SaveSettings()
+        {
+            try
+            {
+                var config = _appConfigService.Load();
+                config.Theme = SelectedTheme.ToString();
+                config.Language = SelectedLanguage.CultureCode;
+                config.Username = Username;
+
+                _appConfigService.Save(config);
+                await _settingsService.SaveGoalAsync(Goal);
+
+                _localizationService.ChangeLanguage(SelectedLanguage.CultureCode);
+                _themeService.SetTheme(SelectedTheme);
+
+                await _messageService.ShowInfoAsync("Msg_SettingsSaved");
+            }
+            catch (AppException ex)
+            {
+                Log.Error(ex, "SettingsVM: Error saving settings");
+                await _messageService.ShowErrorAsync(ex);
+            }
+        }
+
+        private bool CanSaveSettings() => !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Goal);
+
+        [RelayCommand]
+        private async Task ExportEntries()
+        {
+            try
+            {
+                var path = _messageService.SaveFileDialog($"Progress_Backup_{DateTime.Now:yyyyMMdd}", "CSV files (*.csv)|*.csv");
+                if (string.IsNullOrEmpty(path)) return;
+
+                await _dataExchangeService.ExportToCsvAsync(path);
+                await _messageService.ShowInfoAsync("Msg_SuccessExportInfo");
+            }
+            catch (AppException ex)
+            {
+                Log.Error(ex, "SettingsVM: Export failed");
+                await _messageService.ShowErrorAsync(ex);
+            }
+        }
+
+        [RelayCommand]
+        private async Task ImportEntries()
+        {
+            var path = _messageService.OpenFileDialog("CSV files (*.csv)|*.csv");
+            if (string.IsNullOrEmpty(path)) return;
+
+            if (!await _messageService.ShowConfirmationAsync("Msg_ImportConfirmation")) return;
+
+            try
+            {
+                var count = await _dataExchangeService.ImportFromCsvAsync(path);
+                await _messageService.ShowInfoAsync("Msg_SuccessImportInfo", count);
+            }
+            catch (AppException ex)
+            {
+                Log.Error(ex, "SettingsVM: Import failed");
+                await _messageService.ShowErrorAsync(ex);
+            }
+        }
+
+        [RelayCommand]
+        private void TogglePasswordFields() => IsChangingPassword = !IsChangingPassword;
+
+        [RelayCommand(CanExecute = nameof(CanApplyNewPassword))]
+        private async Task ApplyNewPassword()
+        {
+            try
+            {
+                if (IsDbEncrypted)
+                    await _authService.ChangePasswordAsync(NewDbPassword);
+                else
+                {
+                    await _authService.SetPasswordAsync(NewDbPassword);
+                    IsDbEncrypted = true;
+                }
+
+                await _messageService.ShowInfoAsync(IsDbEncrypted ? "Msg_PasswordChangedSuccess" : "Msg_PasswordSetSuccess");
+
+                NewDbPassword = ConfirmDbPassword = string.Empty;
+                IsChangingPassword = false;
+            }
+            catch (AppException ex)
+            {
+                Log.Error(ex, "SettingsVM: Password operation failed");
+                await _messageService.ShowErrorAsync(ex);
+            }
+        }
+
+        private bool CanApplyNewPassword() => !string.IsNullOrWhiteSpace(NewDbPassword) && NewDbPassword == ConfirmDbPassword;
+
+        [RelayCommand(CanExecute = nameof(IsDbEncrypted))]
+        private async Task RemovePassword()
+        {
+            if (!await _messageService.ShowConfirmationAsync("Msg_RemovePasswordConfirmation")) return;
+
+            try
+            {
+                await _authService.RemovePasswordAsync();
+                IsDbEncrypted = false;
+                await _messageService.ShowInfoAsync("Msg_PasswordRemovedSuccess");
+            }
+            catch (AppException ex)
+            {
+                Log.Error(ex, "SettingsVM: Error removing password");
+                await _messageService.ShowErrorAsync(ex);
+            }
+        }
     }
 }
-
